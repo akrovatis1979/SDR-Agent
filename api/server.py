0f3a7e8738a2a575748351e416a2dc2ebab9d979
Konstantinos Galanos <k.galanos@gmail.com>
2026-03-08 20:39:40 +0200

    additional apis

--- api/server.py
+++ api/server.py
@@ -1,6 +1,14 @@
 ###
 import os
 from fastapi import FastAPI
+from .models import ResearchRequest, EmailRequest, ApprovalRequest
+from .rag import RAGTool
+from .workflow import Workflow  
+
+@app.post("/retrieve")
+def retrieve(req: ResearchRequest):
+    docs = rag.retrieve(req.query, k=3)
+    return {"chunks": docs}
 
 @app.post("/generate_email")
 def generate_email(req: EmailRequest):
